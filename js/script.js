8170e39162a0d0c389b1ed3887d8a813a1c4b7a6
manojyadavblack <manojyadavblack@gmail.com>
2023-09-08 12:07:48 +0530

    final commit

--- js/script.js
+++ js/script.js
@@ -39,3 +39,21 @@ var swiper = new Swiper(".review-slider", {
         }
     },
 });
+
+
+
+const spans = document.querySelectorAll('.word span');
+
+spans.forEach((span, idx) => {
+	span.addEventListener('click', (e) => {
+		e.target.classList.add('active');
+	});
+	span.addEventListener('animationend', (e) => {
+		e.target.classList.remove('active');
+	});
+	
+	// Initial animation
+	setTimeout(() => {
+		span.classList.add('active');
+	}, 750 * (idx+1))
+});
